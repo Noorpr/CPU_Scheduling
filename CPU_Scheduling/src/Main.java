c244f84502774ef35c2fca5a2d678756065273bf
b0os <b0oscxs@gmail.com>
2023-12-13 09:57:28 +0200

    Edit Main View

--- CPU_Scheduling/src/Main.java
+++ CPU_Scheduling/src/Main.java
@@ -5,40 +5,75 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int numOfProcesses;
-		ArrayList<Pair<String,Integer>> processes = new ArrayList<Pair<String,Integer>>();
-				// ProcessName, Burst time
-		
+		ArrayList<Pair<String, Integer>> processes = new ArrayList<Pair<String, Integer>>();
+		// ProcessName, Burst time
+
 		System.out.print("Please Enter the number of processes: ");
 		numOfProcesses = scanner.nextInt();
-		
+
 		for (int i = 0; i < numOfProcesses; i++) {
 			String ProcessName;
 			int BurstTime;
-			
+
 			System.out.print("Please Enter the " + (i + 1) + " process name: ");
 			ProcessName = scanner.next();
-			
+
 			System.out.print("Please Enter the " + (i + 1) + " process burst time: ");
 			BurstTime = scanner.nextInt();
-			
+
 			Pair<String, Integer> temp = new Pair<String, Integer>(ProcessName, BurstTime);
-			
+
 			processes.add(temp);
 		}
-		System.out.println("###########################");
-		System.out.print("Please Enter your choice: ");
-		int choice = scanner.nextInt();
-		
-		if (choice == 1) {
-			SJF Scheduler = new SJF(processes);
-			
-			Scheduler.getProcessExecutionOrder();
-			System.out.println();
-			Scheduler.getWaitingTimeForEachProcess();
-			System.out.println();
-			Scheduler.getTurnAroundTimeForEachProcess();
-			System.out.println("Average waiting time: " + Scheduler.getAverageWaitingTime());
-			System.out.println("Average turnaround time: " + Scheduler.getAverageTurnAroundTime());
+
+
+		while(true){
+			System.out.println("\nChoice an algorithm to implement\n" +
+					"1- SJF\n" +
+					"2- SRTF\n" +
+					"3- Priority\n" +
+					"4- AG\n" +
+					"5- Exit");
+			System.out.print("Please Enter your choice: ");
+			int choice = scanner.nextInt();
+
+			switch (choice) {
+
+				case 1:
+					SJF Scheduler = new SJF(processes);
+
+					System.out.println("\n----Implementing SJF----");
+					System.out.println("Process Execution Order:");
+					Scheduler.getProcessExecutionOrder();
+					System.out.println();
+					System.out.println("Waiting Time For Each Process:");
+					Scheduler.getWaitingTimeForEachProcess();
+					System.out.println();
+					System.out.println("TurnAround Time For Each Process:");
+					Scheduler.getTurnAroundTimeForEachProcess();
+					System.out.println("\nAverage waiting time: " + Scheduler.getAverageWaitingTime());
+					System.out.println("Average turnaround time: " + Scheduler.getAverageTurnAroundTime());
+					break;
+
+				case 2:
+					// SRTF
+					System.out.println("\n----Implementing SRTF----");
+					break;
+
+				case 3:
+					// Priority
+					System.out.println("\n----Implementing Priority----");
+					break;
+				case 4:
+					// AG
+					System.out.println("\n----Implementing AG----");
+					break;
+
+				default:
+					System.exit(0);
+
+			}
+
 		}
 	}
 
